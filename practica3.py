07fb9c323af8553f87ee48c1ffd2f01dbfc031ba
JacobArcilaC <47484206+JacobArcilaC@users.noreply.github.com>
2019-05-21 14:42:59 -0500

    Add files via upload

--- practica3.py
+++ practica3.py
@@ -2,33 +2,46 @@ import random, math
 
 #Genera numeros aleatorios entre 1 y 5 
 #usando la funcion de distribucion acumulada 
-def genRandomNumber():
-  randomNumber = random.random() #Genera un numero aleatorio uniforme entre [0, 1] 
-  if(randomNumber <= 0.3):
+def genNumeroAleatorio():
+  numeroAleatorio = random.random() #Genera un numero aleatorio uniforme entre [0, 1] 
+  if(numeroAleatorio <= 0.3):
     return 1
-  elif(randomNumber <= 0.8):
+  elif(numeroAleatorio <= 0.8):
     return 2
-  elif(randomNumber <= 0.9):
+  elif(numeroAleatorio <= 0.9):
     return 3
-  elif(randomNumber <= 0.98):
+  elif(numeroAleatorio <= 0.98):
     return 4
-  elif(randomNumber <= 1):
+  elif(numeroAleatorio <= 1):
     return 5
 
 # PUNTO A. 
 #Generar 1000 numeros aleatorios apartir de la funcion de probabilidad descrita
 def primerPuntoTaller():
-  randomNumbers = []
+  numerosAleatorios = []
   for i in range(0, 1000):
-    randomNumbers.append(genRandomNumber())
-  print("Numeros aleatorios generados ", randomNumbers)
+    numerosAleatorios.append(genNumeroAleatorio())
+  print("Numeros aleatorios generados ", numerosAleatorios)
 
 def segundoPuntoTaller():
-  randomNumbers = []
+  numerosAleatorios = []
   for i in range(0, 1000):
-    randomNumber = random.random() #Genera un numero aleatorio uniforme entre [0, 1]
-    y =  -1*math.log(-1*math.log(randomNumber))
-    randomNumbers.append(y)
-  print("Numeros aleatorios generados ", randomNumbers)
+    numeroAleatorio = random.random() #Genera un numero aleatorio uniforme entre [0, 1]
+    y =  -1*math.log(-1*math.log(numeroAleatorio))
+    numerosAleatorios.append(y)
+  print("Numeros aleatorios generados ", numerosAleatorios)
 
-def tercetPuntoTaller():
+def tercerPuntoTaller():
+  numerosAleatorios = []
+  for i in range(0, 1000):
+    aleatorioExpo =  genNumeroExpo() #Genera un numero aleatorio exponencial entre [0, 1]
+    y =  -1*math.log(-1*math.log(aleatorioExpo))
+    numerosAleatorios.append(y)
+  print("Numeros aleatorios generados ", numerosAleatorios)
+
+def genNumeroExpo():
+  while(True):
+    numeroAleatorio = random.random() #Genera un numero aleatorio uniforme entre [0, 1]
+    aleatorioExpo = -1*math.log(1-numeroAleatorio)
+    if(aleatorioExpo>= 0 and aleatorioExpo<= 1):
+      return numeroAleatorio
